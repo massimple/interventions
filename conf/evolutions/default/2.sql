5123301b65a3685e589ee921ae4320f300728369
afijog <afijo@massimple.es>
2012-07-18 19:15:19 +0200

    Improved evolution DB script to have better dummy data

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,20 +2,24 @@
 
 # --- !Ups
 
-insert into intervention (id,name) values (  1,'Massimple Website');
-insert into intervention (id,name) values (  2,'GitHub signin');
+insert into application (id,name) values (1,'Web Portal');
+insert into application (id,name) values (2,'Web services module');
+insert into application (id,name) values (3,'Back end module');
 
-insert into department (id,name) values (  1,'Unix Admin');
-insert into department (id,name) values (  2,'Windows Admin');
+insert into intervention (id,name,application_id,introduced,scheduled) values (1,'massimple website 2.0 go live',1,
+	PARSEDATETIME('07-07-2012', 'dd-MM-yyyy'),
+	PARSEDATETIME('21-07-2012', 'dd-MM-yyyy'));
 
-insert into application (id,name) values (  1,'Web Portal');
-insert into application (id,name) values (  2,'Web services module');
-insert into application (id,name) values (  3,'Back end module');
+insert into intervention (id,name,application_id,introduced,scheduled) values (2,'massimple business modules', 2,
+	PARSEDATETIME('11-08-2012', 'dd-MM-yyyy'),
+	PARSEDATETIME('25-08-2012', 'dd-MM-yyyy'));
 
-insert into ldeptinterv (idintervention_id, department_id, ) values (1,1);
-insert into ldeptinterv (idintervention_id, department_id, ) values (1,2);
+insert into department (id,name) values (1,'Unix Admin');
+insert into department (id,name) values (2,'Windows Admin');
 
-insert into ldeptinterv (idintervention_id, department_id, ) values (2,1);
+insert into ldeptinterv (idintervention_id, department_id ) values (1,1);
+insert into ldeptinterv (idintervention_id, department_id ) values (1,2);
+insert into ldeptinterv (idintervention_id, department_id ) values (2,1);
 
 # --- !Downs
 
